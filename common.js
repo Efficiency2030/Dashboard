1ae7e680bed1fe522b1b648ce58228a1b7b95ba6
Greenfield2030 <70830738+Greenfield2030@users.noreply.github.com>
2020-09-06 10:34:05 +0530

    Update common.js

--- common.js
+++ common.js
@@ -1,5 +1,6 @@
 D=document;
 B=D.body;
-function CE (type){ return B.createElement(type)}
-function BR (){ return B.createElement("BR")}
-function HR (){ return B.createElement("HR")}
+function CE(type){ return D.createElement(type)}
+function BR(){ return D.createElement("BR")}
+function HR(){ return D.createElement("HR")}
+function AD(obj){ return B.appendChild(obj)}
